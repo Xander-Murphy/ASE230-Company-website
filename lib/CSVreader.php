7faf221fbef55513e96b40c77d8bc44bc9e88bc6
Jack Dixon <jackaverydixon@gmail.com>
2025-10-21 10:27:23 -0400

    made csvreader work

--- lib/CSVreader.php
+++ lib/CSVreader.php
@@ -1,3 +1,26 @@
 <?php
+    function readCSV($filename) {
 
+        echo "<html><body><center><table>\n\n";
+
+        if(!file_exists($filename)) {
+            echo "File not found";
+            return;
+        }
+        
+        $file = fopen($filename, "r");
+        while (($data = fgetcsv($file)) !== false) {
+
+            echo "<tr>";
+            foreach ($data as $i) {
+                echo "<td>" . htmlspecialchars($i) 
+                    . "</td>";
+            }
+            echo "</tr> \n";
+        }
+
+        fclose($file);
+
+        echo "\n</table></center></body></html>";
+    }
 ?>
