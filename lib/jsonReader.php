3001da5e7211e1b8ce1d0aa2a3460daab173eaed
Xander <xanderjm1116@gmail.com>
2025-10-21 18:53:11 -0400

    added and implemented a function to specifically be able to read the applications in products.json, shortened some fields in products.json

--- lib/jsonReader.php
+++ lib/jsonReader.php
@@ -12,4 +12,21 @@
       echo $data[$index][$field];
     }
   }
+  function echoApplicationFromProducts($jsonFile, $index, $appIndex) {
+    if (!file_exists($jsonFile)) {
+        echo "File not found";
+        return;
+    }
+
+    $jsonData = file_get_contents($jsonFile);
+    $data = json_decode($jsonData, true);
+
+    if (!isset($data[$index]['applications'][$appIndex])) {
+        echo "Application not found";
+        return;
+    }
+
+    echo $data[$index]['applications'][$appIndex];
+}
 ?>
+
